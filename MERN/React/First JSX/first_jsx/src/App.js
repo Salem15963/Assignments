9e60c7584e2462179fcf0fb9a5d8757eb75d0338
Omar <Omar.salemps@gmail.com>
2022-12-07 11:15:49 +0200

    First React With JSX commit

---
+++ MERN/React/First JSX/first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 style={{color: 'red'}}>Hello Dojo!</h1>
+        <h2>Things i need to do:</h2>
+        <div style={{color: 'red'}}>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

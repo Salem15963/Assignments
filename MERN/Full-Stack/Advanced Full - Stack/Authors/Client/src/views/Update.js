963c7592eb7c0f85d1c621c13986616f8b5fb52d
Omar <Omar.salemps@gmail.com>
2022-12-26 17:40:25 +0200

    commit

--- MERN/Full-Stack/Advanced Full - Stack/Authors/Client/src/views/Update.js
+++ MERN/Full-Stack/Advanced Full - Stack/Authors/Client/src/views/Update.js
@@ -12,6 +12,7 @@ const Update = (props) => {
     const { id } = useParams();
     const [author, setAuthor] = useState();
     const [loaded, setLoaded] = useState(false);
+    const [errors, setErrors] = useState([]); 
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/authors/' + id)
@@ -25,12 +26,21 @@ const Update = (props) => {
     const updateAuthor = author => {
         axios.put('http://localhost:8000/api/authors/' + id, author)
             .then(res => console.log(res), navigate("/authors/" + id))
-            .catch(err => console.error(err));
+            .catch(err=>{
+                const errorResponse = err.response.data.errors; // Get the errors from err.response.data
+                const errorArr = []; // Define a temp error array to push the messages in
+                for (const key of Object.keys(errorResponse)) { // Loop through all errors and get the messages
+                    errorArr.push(errorResponse[key].message)
+                }
+                // Set Errors
+                setErrors(errorArr);
+            })  
     }
     
     return (
         <div>
             <h1>Update a Author</h1>
+            {errors.map((err, index) => <p key={index}>{err}</p>)}
             {loaded && (
                 <>
                     <AuthorForm
